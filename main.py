b2c8c5cc80f56e1ada6367785d65dbb86a807e33
DeveloperUchida <kouei28poiou@icloud.com>
2024-02-15 11:18:51 +0900

    DataUpload 2024/02/15

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+import time
+
+
+fortunes = ['大吉', '吉', '凶', '大凶']
+fortunes = random.choice(fortunes)
+
+print('おみくじを引きます・・・')
+time.sleep(1)
+for i in range(3):
+    if i % 2 ==0:
+        print('ガザッ')
+    else:
+        print('ゴソッ')
+        time.sleep(1)
+        print('＿人人人人人人人人人＿')
+        print(f'>{fortunes.center(16)}<')
+        print('￣Y^Y^Y^Y^Y^Y^Y^Y^Y￣')
